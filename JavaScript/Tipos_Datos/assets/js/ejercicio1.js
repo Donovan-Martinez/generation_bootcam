d258fba581f50ad9e17875b35db75bafbda9b00c
Donovan_Martinez <ddmartinezcas1311@gmail.com>
2022-04-26 14:41:43 -0500

    Continuacion pagina Bootstrapt

---
+++ JavaScript/Tipos_Datos/assets/js/ejercicio1.js
@@ -0,0 +1,15 @@
+ edad =
+ 
+ function edad(Cadena) {
+        let hoy = new Date()
+        let fechaNacimiento = new Date(Cadena)
+        let edad = hoy.getFullYear() - fechaNacimiento.getFullYear()
+        let diferenciaMeses = hoy.getMonth() - fechaNacimiento.getMonth()
+        if (
+          diferenciaMeses < 0 ||
+          (diferenciaMeses === 0 && hoy.getDate() < fechaNacimiento.getDate())
+        ) {
+          edad--
+        }
+        return edad
+      }
---
+++ JavaScript/Tipos_Datos/assets/js/tiposDatos.js
@@ -0,0 +1,29 @@
+/* alert('tipos de datos') */
+
+/* let nombre = "Donovan"
+let edad = "23"
+const nombreCompleto = "Donovan Martinez";
+
+
+let tinesTrabajo = true;
+let variableNull = null;
+
+let variableIndefinida;
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(tinesTrabajo));
+console.log(typeof(variableNull));
+console.log(typeof(variableIndefinida));
+/*  */
+/* /* if (true){
+    let numero = 10
+}
+    /* console.log(numero); */
+  
+
+/* for(let i=0 i<=10; i++){
+    console.log(i);
+}
+
+console.log(i); */
