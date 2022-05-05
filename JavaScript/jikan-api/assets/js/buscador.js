525a6efd5be58a2f0d160a3c3adb05afaa6b075b
Donovan_Martinez <ddmartinezcas1311@gmail.com>
2022-05-05 09:03:13 -0500

    Realizamos una pagina web anime y agregamos la busqueda de Naruto y One piece

---
+++ JavaScript/jikan-api/assets/js/buscador.js
@@ -0,0 +1,13 @@
+const formulario = document.querySelector("form")
+console.log(formulario);
+
+
+formulario.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    /* console.log("Boton presionar"); */
+
+    const busqueda = document.querySelector("input").value
+    console.log(busqueda);
+
+    window.location.href = `busqueda.html?nombre=${busqueda}`
+})
---
+++ JavaScript/jikan-api/assets/js/mostrarBusqueda.js
@@ -0,0 +1,38 @@
+let busquedaInf = window.location.href.split("=").pop()
+console.log(busquedaInf);
+
+const busquedaAnimes = document.getElementById("busquedaAnimes")
+
+const urlBusqueda = `https://api.jikan.moe/v4/anime?q=${busquedaInf}&order_by=popularity`
+
+fetch(urlBusqueda).then(resp => resp.json()).then(datos =>{
+    console.log(datos.data);
+
+    const resultadoBusqueda = datos.data
+
+    resultadoBusqueda.forEach((resultado) => {
+        
+        const datosNecesarios = {
+            nombre: resultado.title,
+            imagen: resultado.images.jpg.image_url
+        }
+        
+        const template = `<div class="col mb-4">
+                            <div class="card h-100">
+                            <img src="${datosNecesarios.imagen}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                            <h5 class="card-title">${datosNecesarios.nombre}</h5>
+                            
+                            </div>
+                             </div>
+                            </div>`
+
+        busquedaAnimes.innerHTML += template
+
+        
+        console.log(datosNecesarios);
+
+
+
+    })
+})
